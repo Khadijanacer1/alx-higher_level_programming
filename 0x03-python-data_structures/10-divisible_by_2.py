ba468e3a536296ca1fa606f25fd2a300147100ea
Khadijanacer1 <nacerkhadija22@gmail.com>
2023-12-03 10:34:42 -0800

    Only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-
-def max_integer(my_list=[]):
-    maxi = None
-    if len(my_list) > 0:
-        maxi = my_list[0]
-        for item in my_list:
-            if item > maxi:
-                maxi = item
-    return maxi
+def divisible_by_2(my_list=[]):
+    return list(map(lambda x: True if x % 2 == 0 else False, my_list))
